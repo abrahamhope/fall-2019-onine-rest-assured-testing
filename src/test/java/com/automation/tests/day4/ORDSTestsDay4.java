84c23b0c7424d31dee4745d58294233802554258
abraham <abrahamisaachope@gmail.com>
2020-05-23 12:43:09 -0500

    POJO tests added

--- src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
+++ src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
@@ -1,10 +1,13 @@
 package com.automation.tests.day4;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.*;
@@ -44,9 +47,52 @@ public class ORDSTestsDay4 {
  When users sends a GET request to "/countries"
  Then status code is 200
  And Content type is application/json
+ And country_name from payload is "United States of America"
  *
  */
 
+@Test
+@DisplayName("Verify country name, content type and status code for country with ID US")
+public void verifyCountriesTest1() {
+    given().
+            accept(ContentType.JSON).
+            queryParam("q", "{\"country_id\":\"US\"}").
+            when().
+            get("/countries").
+            then().assertThat().
+            statusCode(200).
+            contentType(ContentType.JSON).
+            body("items[0].country_name", is("United States of America"));
+    ///SECOND REQUEST
+    //accept(ContentType.JSON). - to request JSON from the web service.
+    Response response = given().
+            accept(ContentType.JSON).
+            when().
+            get("/countries").prettyPeek();
+    String countryName = response.jsonPath().getString("items.find{it.country_id == 'US'}.country_name");
+    Map<String, Object> countryUS = response.jsonPath().get("items.find{it.country_id == 'US'}");
+    //find all country names from region 2
+    //collectionName.findAll{it.propertyName == 'Value'} -- to get collection objects where property equals to some value
+    //collectionName.find{it.propertyName == 'Value'} -- to object where property equals to some value
+    // to get collection properties where property equals to some value
+    //collectionName.findAll{it.propertyName == 'Value'}.propertyName
+    List<String> countryNames = response.jsonPath().getList("items.findAll{it.region_id == 2}.country_name");
+    System.out.println("Country name: " + countryName);
+    System.out.println(countryUS);
+    System.out.println(countryNames);
+    for (Map.Entry<String, Object> entry : countryUS.entrySet()) {
+        System.out.printf("key = %s, value = %s\n", entry.getKey(), entry.getValue());
+    }
+}
+
+@Test
+public void getEmployeeTest(){
+    Response response = when().get("/employees").prettyPeek();
+    Map<String,?> bestEmployee = response.jsonPath().get("items.max{it.salary}");
+    Map<String, ?> poorGuy = response.jsonPath().get("items.min{it.salary}");
+    System.out.println(bestEmployee);
+    System.out.println(poorGuy);
+    }
 
 
 }
--- src/test/java/com/automation/utilities/ConfigurationReader.java
+++ src/test/java/com/automation/utilities/ConfigurationReader.java
@@ -5,19 +5,21 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
-    private static Properties properties;
+    private static final Properties properties;
 
-    static{
-        try(FileInputStream fileInputStream = new FileInputStream("configuration.properties")){
-            properties = new Properties();
-            properties.load(fileInputStream);
-        }catch (IOException e){
+    static {
+        try {
+            try (FileInputStream fileInputStream = new FileInputStream("configuration.properties")) {
+                properties = new Properties();
+                properties.load(fileInputStream);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to load properties file!");
         }
     }
 
-    public static String getProperties(String propertytName){
-        return properties.getProperty(propertytName);
+    public static String getProperty(String propertyName) {
+        return properties.getProperty(propertyName);
     }
-
 }
